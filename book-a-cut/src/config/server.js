334d86e58f274246665be311aebe1ca9b6d4de0c
Abhinawsah13 <abhinawsah13@gmail.com>
2026-03-31 14:27:27 +0545

    chore: remove temporary test files and update server configuration

--- book-a-cut/src/config/server.js
+++ book-a-cut/src/config/server.js
@@ -1,7 +1,5 @@
 // config/server.js
 
-const IP = '192.168.1.98'; // updated — was 192.168.1.68
-//const IP = '100.64.212.129'; //
-
+const IP = '192.168.1.68';
 export const API_BASE_URL = `http://${IP}:3000/api`;
 export const SOCKET_BASE_URL = `http://${IP}:3000`;
